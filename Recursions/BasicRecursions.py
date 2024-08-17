9d5b6019a830e1baccad53a84e4d9f192f3dca96
vikas965 <mahanthivikas965@gmail.com>
2024-08-17 16:58:22 +0530

    update basic recursions

--- Recursions/BasicRecursions.py
+++ Recursions/BasicRecursions.py
@@ -4,20 +4,20 @@
 
 #fun2 n to 1
 
-# def fun1(n,i):
-#     if i>n:
-#         return
-#     print(i)
-#     fun1(n,i+1)
+def fun1(n,i):
+    if i>n:
+        return
+    print(i)
+    fun1(n,i+1)
 
-# def fun2(n,i):
-#     if n<i:
-#         return
-#     print(n)
-#     fun2(n-1,i)
+def fun2(n,i):
+    if n<i:
+        return
+    print(n)
+    fun2(n-1,i)
 
-# fun1(5,1)
-# fun2(5,1)
+fun1(5,1)
+fun2(5,1)
 
 
 
@@ -42,3 +42,28 @@ def fun4(i,n):
 
 fun3(5,5)
 fun4(1,5)
+
+
+
+# Parametrized and Functional Recursion
+
+# Sum of N numbers using the Parametrized Recursion:
+
+def recsum(n,total):
+    if n<1:
+        print(total)
+        return 
+    recsum(n-1,total+n)
+
+recsum(10,0)
+
+
+# Sum of numbers using the Functional Recursion where we will not pass extra parameter instead we will 
+# return the total 
+
+def parasum(n):
+    if n<1:
+        return 0
+    return n+parasum(n-1)
+
+print(parasum(10))
